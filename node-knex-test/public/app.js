d9213e525e0414fd218fb00c6003ebad99ff176e
Rian Perassoli <rian@torfresma.com.br>
2019-05-31 09:11:05 -0300

    criado add mas nao esta autenticando

--- node-knex-test/public/app.js
+++ node-knex-test/public/app.js
@@ -1,17 +1,31 @@
-const CreateUser = document.querySelector('.CreateUser')
-CreateUser.addEventListener('submit', (e) => {
-  e.preventDefault()
-  const username = CreateUser.querySelector('.username').value
-  const password = CreateUser.querySelector('.password').value
-  post('/createUser', { username, password })
-})
-function post (path, data) {
+const CreateUser = document.querySelector(".CreateUser");
+
+CreateUser.addEventListener("submit", e => {
+  e.preventDefault();
+  const username = CreateUser.querySelector(".username").value;
+  const password = CreateUser.querySelector(".password").value;
+  post("/createUser", { username, password });
+});
+
+const Login = document.querySelector(".Login");
+
+Login.addEventListener("submit", e => {
+  e.preventDefault();
+  const username = Login.querySelector(".username").value;
+  const password = Login.querySelector(".password").value;
+  post("/login", { username, password }).then(({ status }) => {
+    if (status === 200) alert("login success");
+    else alert("login failed");
+  });
+});
+
+function post(path, data) {
   return window.fetch(path, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-  })
+  });
 }
