95f6eaae7771d5841c7e7fcd452b5cc410abb9a0
Rian Perassoli <rian@torfresma.com.br>
2019-05-31 08:41:52 -0300

    alteracao do projeto. Salvando um registro e criada a primeira migration. Utilizado o comando knex migrate:make create_user_table para criar a migration e knex migrate:latest para executá-la

--- node-knex-test/knexfile.js
+++ node-knex-test/knexfile.js
@@ -1,8 +1,10 @@
 module.exports = {
-    client: 'mysql',
-    connection: {
-      user: 'root',
-      password: 'password',
-      database: 'tutorial_node_database'
-    }
+  client: "mysql",
+  connection: {
+    host:
+      "server-registro-de-horas-teste.cdet6beqcke2.us-east-2.rds.amazonaws.com",
+    user: "root",
+    password: "password",
+    database: "server_registro_de_horas_teste"
   }
+};
