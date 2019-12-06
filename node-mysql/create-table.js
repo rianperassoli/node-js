180a8ff23368d1d582949bbf301416ce0d220504
Rian Perassoli <rian@torfresma.com.br>
2019-12-06 10:12:58 -0300

    alterada base

--- node-mysql/create-table.js
+++ node-mysql/create-table.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: 'root',
     password: '',
-    database: 'torfresma'
+    database: 'teste'
 });
 
 function createTable(conn) {
